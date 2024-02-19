5aac459f3c75335d5f04934b7a993b603ff413ca
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-02-19 20:55:21 +0400

    Fixed grass shadows

--- src/xr_3da/xrRender_R2/r2_R_sun.cpp
+++ src/xr_3da/xrRender_R2/r2_R_sun.cpp
@@ -1066,6 +1066,7 @@ void CRender::render_sun_near	()
 			RCache.set_xform_project			(fuckingsun->X.D.combine	);	
 			r_dsgraph_render_graph				(0)	;
 			if (ps_r2_ls_flags.test(R2FLAG_SUN_DETAILS))	
+				Details->UpdateVisibleM			()	;
 				Details->Render					()	;
 			fuckingsun->X.D.transluent			= FALSE;
 			if (bSpecial)						{
